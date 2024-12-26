ed480cb9ee365d976a20d61e3a1b4b5e207d2143
Krish <151437686+Krish-aj@users.noreply.github.com>
2024-12-26 23:28:21 +0530

    Create 4a.py
    f

---
+++ 4a.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+plt.bar([1,3,5,7,9],[5,3,5,7,2],label="data1")
+plt.legend()
+plt.xlabel('x values')
+plt.ylabel('y values')
+plt.title("vertical bar chart")
+plt.show()
