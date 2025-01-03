38c45e45f8c411dc33ce8464fb63037bba9a3eb3
Krish <151437686+Krish-aj@users.noreply.github.com>
2025-01-04 02:11:56 +0530

    Update 1b.py
    r

--- 1b.py
+++ 1b.py
@@ -1,10 +1,10 @@
 val=int(input("enter a number: "))
-str_val=str(val)
-if str_val==str_val[::-1]:
+string=str(val)
+if string==string[::-1]:
     print("palindrome")
 else:
     print("not palindrome")
 
 for i in range(10):
-    if str_val.count(str(i))>0:
-        print(str(i),"appeared",str_val.count(str(i)),"times")
+    if string.count(str(i))>0:
+        print(str(i),"appeared",string.count(str(i)),"times")
