42b9f63882aa8dfce84b78778200aa4db6bfea93
Krish <151437686+Krish-aj@users.noreply.github.com>
2025-01-04 02:13:49 +0530

    Update 2a.py
    rf

--- 2a.py
+++ 2a.py
@@ -1,14 +1,14 @@
-def fibonacci(n):
+def fn(n):
     if n==1:
         return 0;
     elif n==2:
         return 1;
     else:
-        return fibonacci(n-1)+fibonacci(n-2)
+        return fn(n-1)+fn(n-2)
 
 num=int(input("enter the number: "))
 if num>0:
-    result=fibonacci(num)
-    print("fibonacci of ",num," is ",result)
+    result=fn(num)
+    print("fn(",num,") is ",result)
 else:
     print("error in input")
