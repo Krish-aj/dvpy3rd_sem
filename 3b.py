286a47e4cd1001897f6d4c3a3e93b739b4d4b2da
Krish <151437686+Krish-aj@users.noreply.github.com>
2025-01-04 02:48:47 +0530

    Update 3b.py
    hg

--- 3b.py
+++ 3b.py
@@ -1,15 +1,10 @@
-def compare(s1,s2):
-    count=0
-    n=min(len(s1),len(s2))
-    for i in range(n):
-        if(s1[i]==s2[i]):
-            count+=1;
-    return count
-    
-s1=input("enter string 1: ")
-s2=input("enter string 2: ")
-count=compare(s1,s2)
-mx=max(len(s1),len(s2))
-match=count/mx*100
-print("letters match is: ",count)
-print("match percent: ",match)
+str1=input("enter str1: ")
+str2=input("enter str2: ")
+shortstring=min(len(str1),len(str2))
+longstring=max(len(str1),len(str2))
+count=0
+for i in range(shortstring):
+    if(str1[i]==str2[i]):
+        count+=1
+print("letters matched is :",count)
+print("match percent is: ",(count/longstring)*100)
